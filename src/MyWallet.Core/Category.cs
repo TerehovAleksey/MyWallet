e29443a7c1c09d9551264bfd92d4ab8795b789ba
TerehovAleksei <375295565021@yandex.ru>
2022-11-05 19:34:25 +0300

    updated CategoryController, Category Entity

--- src/MyWallet.Core/Category.cs
+++ src/MyWallet.Core/Category.cs
@@ -4,6 +4,8 @@ public class Category
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = default!;
+    public bool IsVisible { get; set; } = true;
+    public string? ImageName { get; set; }
 
     public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
 }
