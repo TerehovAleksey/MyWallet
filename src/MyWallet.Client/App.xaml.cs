69b63766f336060e26ce15d3830a37a980b621a2
TerehovAleksei <375295565021@yandex.ru>
2022-12-05 14:37:10 +0300

    dev maui-client

--- src/MyWallet.Client/App.xaml.cs
+++ src/MyWallet.Client/App.xaml.cs
@@ -10,8 +10,15 @@ public partial class App : Application
         var cache = ServiceHelpers.GetService<IBarrel>();
         if (cache is not null)
         {
-            //test clean
-            //cache.EmptyAll();
+#if DEBUG
+            //очистка кеша, если изменился источник данных
+            var source = cache.Get<string>("ApiServiceUrl");
+            if (string.IsNullOrEmpty(source) || source != Constants.ApplicationId) 
+            {
+                cache.EmptyAll();
+            }
+            cache.Add("ApiServiceUrl", Constants.ApplicationId, TimeSpan.MaxValue);
+#endif
 
             var token = cache.Get<string>(Constants.TOKEN_KEY);
             if (string.IsNullOrEmpty(token))
